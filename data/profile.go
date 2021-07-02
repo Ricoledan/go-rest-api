0b64b233ec13c53658d40461720d37bdb6bf3101
Ricardo Ledan <ricoledan@gmail.com>
2021-07-01 21:49:25 -0400

    add profile data

--- data/profile.go
+++ data/profile.go
@@ -1,7 +1,9 @@
 package data
 
+import "time"
+
 type Technique struct {
-	Name string
+	Name        string
 	Description string
 }
 
@@ -13,7 +15,7 @@ type Profile struct {
 	Attribute string
 	Field     []string
 	Group     []string
-	Technique []Technique 
+	Technique []Technique
 	Artwork   string
 	Profile   string
 	CreatedAt string
@@ -23,11 +25,21 @@ type Profile struct {
 
 var profileList = []*Profile{
 	&Profile{
-		ID: 1,
-		Name: "Agumon",
-		Level: "Child",
-		Type: "Reptile",
+		ID:        1,
+		Name:      "Agumon",
+		Level:     "Child",
+		Type:      "Reptile",
 		Attribute: "Vaccine",
-		Field: ["Metal Empire","Nature Spirits","Virus Busters","Unknown","Dragon's Roar","Nightmare Soldiers"]
+		Field:     "Metal Empire", "Nature Spirits", "Virus Busters", "Unknown", "Dragon's Roar", "Nightmare Soldiers",
+		Group: nil,
+		Technique: {
+			Name:        "Baby Flame",
+			Description: "Releases a stream of fire from its mouth.",
+		},
+		Artwork:   "https://wikimon.net/images/7/72/Agumon.jpg",
+		Profile:   "A Reptile Digimon with an appearance resembling a small dinosaur, it has grown and become able to walk on two legs. Its strength is weak as it is still in the process of growing, but it has a fearless and rather ferocious personality. Hard, sharp claws grow from both its hands and feet, and their power is displayed in battle. It also foreshadows an evolution into a great and powerful Digimon. Its Special Move is spitting a fiery breath from its mouth to attack the opponent",
+		CreatedAt: time.Now().UTC().String(),
+		UpdatedAt: time.Now().UTC().String(),
+		DeletedAt: time.Now().UTC().String(),
 	}
 }
