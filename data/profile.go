d077097f62d24e75b70cd0e26031375eae604256
Ricardo Ledan <ricoledan@gmail.com>
2021-07-01 21:54:55 -0400

    wip

--- data/profile.go
+++ data/profile.go
@@ -13,7 +13,7 @@ type Profile struct {
 	Level     string
 	Type      string
 	Attribute string
-	Field     []string
+	Field     [string]
 	Group     []string
 	Technique []Technique
 	Artwork   string
@@ -32,7 +32,7 @@ var profileList = []*Profile{
 		Attribute: "Vaccine",
 		Field:     "Metal Empire", "Nature Spirits", "Virus Busters", "Unknown", "Dragon's Roar", "Nightmare Soldiers",
 		Group: nil,
-		Technique: {
+		Technique: []*Technique{
 			Name:        "Baby Flame",
 			Description: "Releases a stream of fire from its mouth.",
 		},
@@ -40,6 +40,22 @@ var profileList = []*Profile{
 		Profile:   "A Reptile Digimon with an appearance resembling a small dinosaur, it has grown and become able to walk on two legs. Its strength is weak as it is still in the process of growing, but it has a fearless and rather ferocious personality. Hard, sharp claws grow from both its hands and feet, and their power is displayed in battle. It also foreshadows an evolution into a great and powerful Digimon. Its Special Move is spitting a fiery breath from its mouth to attack the opponent",
 		CreatedAt: time.Now().UTC().String(),
 		UpdatedAt: time.Now().UTC().String(),
-		DeletedAt: time.Now().UTC().String(),
-	}
+	},
+	&Profile{
+		ID:        2,
+		Name:      "Agumon",
+		Level:     "Child",
+		Type:      "Reptile",
+		Attribute: "Vaccine",
+		Field:     "Metal Empire", "Nature Spirits", "Virus Busters", "Unknown", "Dragon's Roar", "Nightmare Soldiers",
+		Group: nil,
+		Technique: []*Technique{
+			Name:        "Baby Flame",
+			Description: "Releases a stream of fire from its mouth.",
+		},
+		Artwork:   "https://wikimon.net/images/7/72/Agumon.jpg",
+		Profile:   "A Reptile Digimon with an appearance resembling a small dinosaur, it has grown and become able to walk on two legs. Its strength is weak as it is still in the process of growing, but it has a fearless and rather ferocious personality. Hard, sharp claws grow from both its hands and feet, and their power is displayed in battle. It also foreshadows an evolution into a great and powerful Digimon. Its Special Move is spitting a fiery breath from its mouth to attack the opponent",
+		CreatedAt: time.Now().UTC().String(),
+		UpdatedAt: time.Now().UTC().String(),
+	},
 }
